520f9873620dec410c4e9f14d0f3bedc04f3952e
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2026-03-05 19:41:03 -0600

    Added movement set & simple speed up effect

---
+++ Source/GASProject/GAS/AttributeSets/MovementSet.cpp
@@ -0,0 +1,18 @@
+﻿#include "MovementSet.h"
+
+UMovementSet::UMovementSet() : Speed(600.0f)
+{
+	MinSpeed = 0.0f;
+}
+
+void UMovementSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+	
+	if (GetSpeed() < MinSpeed)
+	{
+		SetSpeed(MinSpeed);
+	}
+	
+	OnSpeedChanged.Broadcast(GetSpeed());
+}
---
+++ Source/GASProject/GAS/AttributeSets/MovementSet.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "MovementSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSpeedChangedDelegate, float, NewSpeed);
+
+UCLASS()
+class GASPROJECT_API UMovementSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	ATTRIBUTE_ACCESSORS(UMovementSet, Speed);
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnSpeedChangedDelegate OnSpeedChanged;
+
+	UMovementSet();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayAttributeData Speed;
+	
+	float MinSpeed;
+	
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+};
