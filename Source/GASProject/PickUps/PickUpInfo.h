c05362f88ae2b3cf64f86b26daff0739a02e35e2
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2026-03-05 22:14:04 -0600

    Added pick up object & its widget

---
+++ Source/GASProject/PickUps/PickUpInfo.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PickUpInfo.generated.h"
+
+
+class UGameplayEffect;
+
+UCLASS()
+class GASPROJECT_API UPickUpInfo : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UGameplayEffect* PickUpEffect;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText PickUpText;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bHasDuration;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (EditCondition = "bHasDuration"))
+	float Duration;
+};
