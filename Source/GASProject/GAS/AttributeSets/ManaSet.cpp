6fc86ca1d7284fa409fd60e68ddfdd2044db790e
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2026-03-12 21:59:02 -0500

    Added combat animations pack

---
+++ Source/GASProject/GAS/AttributeSets/ManaSet.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ManaSet.h"
+
+UManaSet::UManaSet() : Mana(100), MaxMana(100.0f) 
+{
+	MinMana = 0;
+}
+
+void UManaSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+	
+	if (Attribute == GetManaAttribute())
+	{
+		float NewMana = FMath::Clamp(NewValue, MinMana, GetMaxMana());
+		SetMana(NewMana);
+	}
+}
---
+++ Source/GASProject/GAS/AttributeSets/ManaSet.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "ManaSet.generated.h"
+
+
+UCLASS()
+class GASPROJECT_API UManaSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UManaSet();
+	
+	ATTRIBUTE_ACCESSORS_BASIC(UManaSet, Mana);
+	ATTRIBUTE_ACCESSORS_BASIC(UManaSet, MaxMana);
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData MaxMana;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+protected:
+	float MinMana;
+};
