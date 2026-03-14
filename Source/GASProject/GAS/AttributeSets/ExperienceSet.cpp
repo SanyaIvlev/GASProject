58b88bc6b9807c50ea012b9f1c5e4d2dbef40f9b
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2026-03-13 20:01:49 -0500

    Added experience bar

---
+++ Source/GASProject/GAS/AttributeSets/ExperienceSet.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExperienceSet.h"
+
+UExperienceSet::UExperienceSet() : Level(0), Experience(0), NeededExperience(1.5f * 1 * 1 - 1.5f * 1 + 1)
+{
+	StartingLevel = 0;
+}
+
+void UExperienceSet::CalculateNeededExperience()
+{
+	float NextLevel = GetLevel();
+	SetNeededExperience(1.5f * NextLevel * NextLevel - 1.5f * NextLevel + 1);
+}
+
+void UExperienceSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+	
+	if (Attribute == GetLevelAttribute() && NewValue < StartingLevel)
+	{
+		NewValue = StartingLevel;
+	}
+}
+
+void UExperienceSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+	
+	if (Attribute == GetExperienceAttribute())
+	{
+		if (NewValue >= GetNeededExperience())
+		{
+			SetLevel(GetLevel() + 1);
+			SetExperience(GetNeededExperience() - NewValue);
+			
+			CalculateNeededExperience();
+		}
+	}
+	else if (Attribute == GetLevelAttribute())
+	{
+		CalculateNeededExperience();
+	}
+}
---
+++ Source/GASProject/GAS/AttributeSets/ExperienceSet.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "ExperienceSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASPROJECT_API UExperienceSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	ATTRIBUTE_ACCESSORS_BASIC(UExperienceSet, Level)
+	ATTRIBUTE_ACCESSORS_BASIC(UExperienceSet, Experience)
+	
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(UExperienceSet, NeededExperience)
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(NeededExperience) 
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(NeededExperience)
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Level;
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Experience;
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData NeededExperience;
+	
+	UExperienceSet();
+	
+	void CalculateNeededExperience();
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	
+protected:
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(NeededExperience)
+	
+	float StartingLevel;
+};
